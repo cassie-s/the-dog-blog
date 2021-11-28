212fbc2c1692a89ee8a60d87d4fdb8b747b4f548
Aisha Osman <aisha.osman09@gmail.com>
2021-11-28 16:43:16 -0600

    spacing

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,11 +3,4 @@ VALUES
 ('Henry', 3),
 ('Snowball', 7),
 ('Mayo', 4),
-<<<<<<< HEAD
 ('Janet', 2);
-=======
-('Janet', 2),
-('Furry', 4),
-('James', 2);
-
->>>>>>> 4178cff5f69ad13c1ef645d965598796d823348d
