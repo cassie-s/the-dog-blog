87c7ebfe21c08fec53449b8bb935693fccc17fc7
EmilyT <emilython@hotmail.com>
2021-11-29 12:11:00 -0600

    added password encryption

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,7 +7,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll()
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
@@ -18,6 +20,7 @@ router.get('/', (req, res) => {
   // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+      attributes: { exclude: ['password'] },
       where: {
         id: req.params.id
       }
@@ -50,12 +53,35 @@ router.post('/', (req, res) => {
       });
   });
 
+  router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
   // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
+      individualHooks: true,
       where: {
         id: req.params.id
       }
--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 // Dependencies
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -35,7 +41,20 @@ User.init(
       }
     }
   },
+
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     // Table configuration
     sequelize,
     timestamps: false,
