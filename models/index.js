71aa9f4da8f843c46d279533b808f1656241cabf
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2021-11-29 10:17:43 -0600

    model index.js file completed

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const { model } = require('../config/connection');
+const Vote = require('./Vote');
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote};
