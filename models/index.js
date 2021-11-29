f9ba77454a7f718dc2e9b7c6340016e23d22016c
EmilyT <emilython@hotmail.com>
2021-11-29 12:58:40 -0600

    added comment model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // const Vote = require('./Vote');
  const User = require('./User');
  const Post = require('./Post');
-// const Comment = require('./Comment');
+ const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -40,20 +40,20 @@ Post.belongsTo(User, {
 //     foreignKey: 'post_id'
 //   });
 
-//   Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-//   });
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
   
-//   Comment.belongsTo(Post, {
-//     foreignKey: 'post_id'
-//   });
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
   
-//   User.hasMany(Comment, {
-//     foreignKey: 'user_id'
-//   });
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
   
-//   Post.hasMany(Comment, {
-//     foreignKey: 'post_id'
-//   });
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
 
- module.exports = { User, Post };
+ module.exports = { User, Post, Comment };
