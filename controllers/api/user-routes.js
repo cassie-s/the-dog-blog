a8ac6ba148f508fbae92b655d0e064cb6e64847a
EmilyT <emilython@hotmail.com>
2021-11-29 19:21:53 -0600

    added logout button

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -105,6 +105,17 @@ router.post('/', (req, res) => {
     });
   });
 
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+
   // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
