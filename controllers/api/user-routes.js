1875eb897dbdc24a9f156171d4df79aa71719c87
EmilyT <emilython@hotmail.com>
2021-11-29 19:17:01 -0600

    added login functionality, cookies, will redirect to homepage if logged in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,7 +61,15 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
+      });
+    })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -69,7 +77,6 @@ router.post('/', (req, res) => {
   });
 
   router.post('/login', (req, res) => {
-    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
       where: {
         email: req.body.email
@@ -81,12 +88,20 @@ router.post('/', (req, res) => {
       }
   
       const validPassword = dbUserData.checkPassword(req.body.password);
+  
       if (!validPassword) {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
   
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
   });
 
@@ -106,6 +121,7 @@ router.put('/:id', (req, res) => {
           res.status(404).json({ message: 'No user found with this id' });
           return;
         }
+
         res.json(dbUserData);
       })
       .catch(err => {
