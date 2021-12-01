f08c7c11aec801ad5e1d20b25db609870c5717f1
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-11-30 20:14:13 -0600

    update html pages to reference homepage.handlebars

---
+++ controllers/api/henry-routes.js
@@ -0,0 +1,107 @@
+// Dependencies
+const router = require('express').Router();
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
+const sequelize = require('../../config/connection');
+const { route } = require('..');
+
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+      attributes: ['id', 
+                   'post_text',
+                   'title',
+                   'created_at',
+                   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),'vote_count']
+                ],
+      // show latest news first
+      order: [['created_at', 'DESC']],
+      // JOIN to the User table
+      include: [
+          {
+          model: User, 
+          attributes: ['username']
+          }
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+  router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 
+                   'post_url', 
+                   'title',
+                   'created_at',
+                   [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),'vote_count']
+                ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+// UPDATE a post
+router.put('/upVote', (req, res) => {
+  if (req.session){
+  Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+  .then(updatedVoteData => res.json(updatedVoteData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+}
+});
