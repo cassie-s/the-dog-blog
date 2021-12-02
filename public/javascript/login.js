4cf12095c201b292b1b163406487acf5c6c59ad1
EmilyT <emilython@hotmail.com>
2021-12-01 19:45:59 -0600

    signup form console logs 'success' if successful

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,4 @@
-
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector('#username-signup').value.trim();
@@ -7,7 +6,7 @@ function signupFormHandler(event) {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && email && password) {
-    fetch('/api/users', {
+    const response = await fetch('/api/users', {
       method: 'post',
       body: JSON.stringify({
         username,
@@ -15,7 +14,14 @@ function signupFormHandler(event) {
         password
       }),
       headers: { 'Content-Type': 'application/json' }
-    }).then((response) => {console.log(response)})
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log('success');
+    } else {
+      alert(response.statusText);
+    }
   }
 }
 
