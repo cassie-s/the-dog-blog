48ee8bf8925b8c77df236dfed2c16ad138e4d4fe
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-12-02 19:04:20 -0600

    fix query selectors

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,32 +1,32 @@
-// signup function
-async function signupFormHandler(event) {
-  event.preventDefault();
+// // signup function
+// async function signupFormHandler(event) {
+//   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+//   const username = document.querySelector('#username-signup').value.trim();
+//   const email = document.querySelector('#email-signup').value.trim();
+//   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+//   if (username && email && password) {
+//     const response = await fetch('/api/users', {
+//       method: 'post',
+//       body: JSON.stringify({
+//         username,
+//         email,
+//         password
+//       }),
+//       headers: { 'Content-Type': 'application/json' }
+//     });
 
-    // check the response status
-    if (response.ok) {
-      console.log('success');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
+//     // check the response status
+//     if (response.ok) {
+//       console.log('success');
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+// document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 
 // login function 
 async function loginFormHandler(event) {
