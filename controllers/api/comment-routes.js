a67bc0ab2159b9babe5101165ad6cfcde6b09847
EmilyT <emilython@hotmail.com>
2021-12-04 21:11:48 -0600

    commented out everything that has to do with post

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,8 +13,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+        user_id: req.body.user_id
       })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
