1d5491e43e691548e5d2eeb2c2d2ac31c18d80eb
EmilyT <emilython@hotmail.com>
2021-12-05 21:13:46 -0600

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
- const sequelize = require('sequelize');
-
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -15,7 +13,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
-
-
 
 module.exports = sequelize;
